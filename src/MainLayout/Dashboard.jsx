f49c1d7cbdbef92217d38e9bea67d52e310d200d
Ariful4a <afkgamingno1@gmail.com>
2025-08-12 22:41:51 +0600

    add User dashboard system

--- src/MainLayout/Dashboard.jsx
+++ src/MainLayout/Dashboard.jsx
@@ -1,21 +1,50 @@
 import { NavLink, Outlet } from "react-router";
-import { FaShoppingCart } from "react-icons/fa";
+import { FaCalendarAlt, FaEnvelope, FaHome, FaShoppingBag, FaShoppingCart } from "react-icons/fa";
+import { MdPayment, MdReviews } from "react-icons/md";
+import { TbBrandBooking } from "react-icons/tb";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const Dashboard = () => {
     return (
-        <div className="flex">
+        <div className="flex container mx-auto">
             {/* dashboard side bar  */}
-            <div className="w-64 min-h-screen bg-orange-500 rounded-2xl">
-                <ul className="menu p-4">
-                    <li>
-                        
+            <div className="w-64 min-h-screen bg-success rounded-2xl">
+                <ul className="menu p-4 w-full space-y-4">
+                    <li className="bg-[#32327d] text-white rounded w-full font-black">
+                        <NavLink to={'/dashboard/cart'}><FaHome />User Home</NavLink>
+                    </li>
+                    <li className="bg-[#32327d] text-white rounded w-full">
+                        <NavLink to={'/dashboard/cart'}><FaCalendarAlt />Reservation</NavLink>
+                    </li>
+                    <li className="bg-[#32327d] text-white rounded w-full">
+                        <NavLink to={'/dashboard/cart'}><MdPayment />Payment History</NavLink>
+                    </li>
+                    <li className="bg-[#32327d] text-white rounded w-full">
                         <NavLink to={'/dashboard/cart'}><FaShoppingCart /> My cart</NavLink>
+                    </li>
+                    <li className="bg-[#32327d] text-white rounded w-full">
+                        <NavLink to={'/dashboard/cart'}><MdReviews />Add Review</NavLink>
+                    </li>
+                    <li className="bg-[#32327d] text-white rounded w-full">
+                        <NavLink to={'/dashboard/cart'}><TbBrandBooking />My Booking</NavLink>
+                    </li>
+                    <li className="bg-[#32327d] text-white rounded w-full">
+                        <NavLink to={'/'}><FaHome />Home</NavLink>
+                    </li>
+                    <li className="bg-[#32327d] text-white rounded w-full">
+                        <NavLink to={'/dashboard/cart'}><GiHamburgerMenu />Menu</NavLink>
+                    </li>
+                    <li className="bg-[#32327d] text-white rounded w-full">
+                        <NavLink to={'/dashboard/cart'}><FaShoppingBag />Shop</NavLink>
+                    </li>
+                    <li className="bg-[#32327d] text-white rounded w-full">
+                        <NavLink to={'/dashboard/cart'}><FaEnvelope />Contact</NavLink>
                     </li>
                 </ul>
             </div>
 
             {/* Dashboard content  */}
-            <div className="flex-1">
+            <div className="flex-1 p-4">
                 <Outlet></Outlet>
             </div>
         </div>
--- src/Pages/Dashboard/Cart/Cart.jsx
+++ src/Pages/Dashboard/Cart/Cart.jsx
@@ -1,10 +1,92 @@
+import useCart from "../../AddToCart/useCart";
+
 
 const Cart = () => {
-    return (
-        <div>
-            <h1>My Cart</h1>
+  const [cart] = useCart();
+  const totalPrice = cart.reduce( (total, item) => total + item.price ,0);
+
+  return (
+    <div>
+      <div className="flex flex-col text-center justify-center">
+        {/* Cart Title */}
+        <h2 className="text-amber-500 font-medium text-2xl">---My Cart---</h2>
+
+        {/* Divider before text */}
+        <div className="flex justify-center">
+          <div className="divider divider-secondary w-[fit-content] min-w-[460px]"></div>
         </div>
-    );
+
+        {/* Main Heading */}
+        <h1 className="text-5xl font-black">WANNA ADD MORE?</h1>
+
+        {/* Divider after text */}
+        <div className="flex justify-center">
+          <div className="divider divider-secondary w-[fit-content] min-w-[460px]"></div>
+        </div>
+      </div>
+
+      {/* Total price */}
+      <div className="flex justify-evenly mb-10">
+        <h2 className="text-4xl font-black">TOTAL ORDER: #{cart.length}</h2>
+        <h2 className="text-4xl font-black">TOTAL PRICE: ${totalPrice}</h2>
+        <button className="btn btn-success">PAY</button>
+      </div>
+
+      {/* Table with custom header */}
+      <div>
+        <div className="overflow-x-auto">
+          <table className="table">
+            {/* head */}
+            <thead>
+              <tr className="bg-success">
+                <th>
+                  #
+                </th>
+                <th>Item Image</th>
+                <th>Item Name</th>
+                <th>Price</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                cart.map(item => <tr key={item._id}>
+                <th>
+                 #
+                </th>
+                <td>
+                  <div className="flex items-center gap-3">
+                    <div className="avatar">
+                      <div className="mask mask-squircle h-12 w-12">
+                        <img
+                          src={item.image}
+                          alt={item.name}
+                        />
+                      </div>
+                    </div>
+                    <div>
+                      <div className="font-bold">Hart Hagerty</div>
+                      <div className="text-sm opacity-50">United States</div>
+                    </div>
+                  </div>
+                </td>
+                <td>
+                 {item.name}
+                </td>
+                <td>{item.price}</td>
+                <th>
+                  <button className="btn btn-error btn-xs">Delete</button>
+                </th>
+              </tr>
+              )
+              }
+            
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
